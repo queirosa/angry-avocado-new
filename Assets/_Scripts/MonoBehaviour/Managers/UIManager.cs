a0dcd12b533e60c1443fe93bebfbfa27ce24436d
hossam300 <semsem9000@gmail.com>
2022-05-08 07:03:06 +0200

    apply year timer & Orginize SO

--- Assets/_Scripts/MonoBehaviour/Managers/UIManager.cs
+++ Assets/_Scripts/MonoBehaviour/Managers/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class UIManager : StaticInstance<UIManager>
 {
@@ -13,21 +14,35 @@ public class UIManager : StaticInstance<UIManager>
     public TextMeshProUGUI oxygenText;
     public TextMeshProUGUI yearsText;
 
-
+    private int currentRate;
+    PlayerStats playerStats;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerStats = GameManager.Instance.PlayerStats;
+        currentRate = playerStats.yearRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        waterText.text = GameManager.Instance.PlayerStats.water.ToString();
-        seedCountText.text = GameManager.Instance.PlayerStats.seedCount.ToString();
-        fertilityText.text = GameManager.Instance.PlayerStats.fertility.ToString();
-        oxygenText.text = GameManager.Instance.PlayerStats.oxygen.ToString();
-        yearsText.text = GameManager.Instance.PlayerStats.years.ToString();
+        waterText.text = playerStats.water.ToString();
+        seedCountText.text = playerStats.seedCount.ToString();
+        fertilityText.text = playerStats.fertility.ToString();
+        oxygenText.text = playerStats.oxygen.ToString();
+        yearsText.text = playerStats.years.ToString();
+        CalculateYearLeft();
+    }
+
+    private void CalculateYearLeft()
+    {
+        if (currentRate <= 0)
+        {
+            GameManager.Instance.PlayerStats.years--;
+            currentRate = playerStats.yearRate;
+        }
+        else
+            currentRate--;
     }
 }
--- Assets/_Scripts/ScriptableObjects/PlayerStats.cs
+++ Assets/_Scripts/ScriptableObjects/PlayerStats.cs
@@ -6,98 +6,163 @@ using UnityEngine;
 
 public class PlayerStats : ScriptableObject
 {
+    [Header("Years")]
+    [Range(0, 10000)]
     public int years;
-    public int water;
-    public int seedCount;
-    public int oxygen;
-    public int fertility;
-
-    public int numberOfDrones;
-
-    public int numberOfActiveDrones;
-
+    [Range(0, 10000)]
     public int maxYears;
+    [Range(0, 10000)]
+    public int yearRate;
+
+    [Header("Water")]
+    [Range(0, 10000)]
+    public int water;
+    [Range(0, 10000)]
     public int maxWater;
-    public int maxOxygen;
+
+    [Header("Seeds")]
+    [Range(0, 10000)]
+    public int seedCount;
+    [Range(0, 10000)]
     public int maxSeedCount;
+
+    [Header("Oxygen")]
+    [Range(0, 10000)]
+    public int oxygen;
+    [Range(0, 10000)]
+    public int maxOxygen;
+
+    [Header("Fertility")]
+    [Range(0, 10000)]
+    public int fertility;
+    [Range(0, 10000)]
     public int maxFertility;
 
-    public void addYears(int yearsToAdd) {
-        if(years + yearsToAdd <= maxYears) {
+    [Header("Drones")]
+    [Range(0, 10000)]
+    public int numberOfDrones;
+    [Range(0, 10000)]
+    public int numberOfActiveDrones;
+
+
+
+
+    public void AddYears(int yearsToAdd)
+    {
+        if (years + yearsToAdd <= maxYears)
+        {
             years += yearsToAdd;
-        } else {
+        }
+        else
+        {
             years = maxYears;
         }
     }
 
-    public void addWater(int waterToAdd) {
-        if(years + waterToAdd <= maxWater) {
+    public void AddWater(int waterToAdd)
+    {
+        if (years + waterToAdd <= maxWater)
+        {
             water += waterToAdd;
-        } else {
+        }
+        else
+        {
             water = maxWater;
         }
     }
 
-    public void addOxygen(int oxygenToAdd) {
-        if(years + oxygenToAdd <= maxOxygen) {
+    public void AddOxygen(int oxygenToAdd)
+    {
+        if (years + oxygenToAdd <= maxOxygen)
+        {
             oxygen += oxygenToAdd;
-        } else {
+        }
+        else
+        {
             oxygen = maxOxygen;
         }
     }
 
-    public void addFertility(int fertilityToAdd) {
-        if(years + fertilityToAdd <= maxYears) {
+    public void AddFertility(int fertilityToAdd)
+    {
+        if (years + fertilityToAdd <= maxYears)
+        {
             fertility += fertilityToAdd;
-        } else {
+        }
+        else
+        {
             fertility = maxFertility;
         }
     }
 
-    public void addSeedCount(int seedCountToAdd) {
-        if(seedCount + seedCountToAdd <= maxSeedCount) {
+    public void AddSeedCount(int seedCountToAdd)
+    {
+        if (seedCount + seedCountToAdd <= maxSeedCount)
+        {
             seedCount += seedCountToAdd;
-        } else {
+        }
+        else
+        {
             seedCount = maxSeedCount;
         }
     }
 
-    public void subYears(int yearsToSub) {
-        if(years - yearsToSub >= 0) {
+    public void SubYears(int yearsToSub)
+    {
+        if (years - yearsToSub >= 0)
+        {
             years -= yearsToSub;
-        } else {
+        }
+        else
+        {
             years = 0;
         }
     }
 
-    public void subWater(int waterToSub) {
-        if(water - waterToSub >= 0) {
+    public void SubWater(int waterToSub)
+    {
+        if (water - waterToSub >= 0)
+        {
             water -= waterToSub;
-        } else {
+        }
+        else
+        {
             water = 0;
         }
     }
 
-    public void subOxygen(int oxygenToSub) {
-        if(oxygen - oxygenToSub >= 0) {
+    public void SubOxygen(int oxygenToSub)
+    {
+        if (oxygen - oxygenToSub >= 0)
+        {
             oxygen -= oxygenToSub;
-        } else {
+        }
+        else
+        {
             oxygen = 0;
         }
     }
 
-    public void subFertility(int fertilityToSub) {
-        if(fertility - fertilityToSub >= 0) {
+    public void SubFertility(int fertilityToSub)
+    {
+        if (fertility - fertilityToSub >= 0)
+        {
             fertility -= fertilityToSub;
-        } else {
+        }
+        else
+        {
             fertility = 0;
         }
     }
 
-    public void subSeedCount(int seedCountToSub) {
-        if(seedCount - seedCountToSub >= 0) {
+    public void SubSeedCount(int seedCountToSub)
+    {
+        if (seedCount - seedCountToSub >= 0)
+        {
             seedCount -= seedCountToSub;
-        } else {
+        }
+        else
+        {
             seedCount = 0;
         }
     }
